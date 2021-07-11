4241d45a66d8fe70e1642468be189aff6ad25837
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-07-11 10:01:54 +0000

    Fix build breakage.

--- src/App.tsx
+++ src/App.tsx
@@ -2,4 +2,6 @@ import React, { useEffect, useState } from "react"
 import { Button, ButtonToolbar, Card, Container, Jumbotron } from "react-bootstrap"
 import { Api } from "./api"
 import "./App.css"
-import "./bootstrap.
+import "./bootstrap.min.css"
+import { Colors } from "./Colors"
+import { Header } from "
