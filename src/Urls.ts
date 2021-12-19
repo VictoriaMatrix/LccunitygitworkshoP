8e0181a36ba0b95a6a4f24b7186c6555673cc614
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-19 14:54:54 +0000

    Update api.lua

--- src/Urls.ts
+++ src/Urls.ts
@@ -2,4 +2,4 @@ import {Secret} from "./secret"
 import {convertDate} from "./utils"
 
 export const URLS = {
-    history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.c
+    history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)
