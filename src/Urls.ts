80ec53f813de552ff0cb4091218797ba2747f129
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-19 20:59:38 +0000

    Translated using Weblate (Urdu)

--- src/Urls.ts
+++ src/Urls.ts
@@ -2,4 +2,5 @@ import {Secret} from "./secret"
 import {convertDate} from "./utils"
 
 export const URLS = {
-    history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)
+    history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}`,
+    metrics: (stock: string) => `https://f
