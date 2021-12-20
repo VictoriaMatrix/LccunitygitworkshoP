f549b4c431440bb2188c213ffc8d262a72eb895a
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-20 11:54:31 +0000

    fixed counter

--- src/Urls.ts
+++ src/Urls.ts
@@ -3,4 +3,5 @@ import {convertDate} from "./utils"
 
 export const URLS = {
     history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}`,
-    metrics: (stock: string) => `https://financialmodelingprep.com/api/v3/company-key-metrics/${stock}?period=quarter
+    metrics: (stock: string) => `https://financialmodelingprep.com/api/v3/company-key-metrics/${stock}?period=quarter`,
+    news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${s
