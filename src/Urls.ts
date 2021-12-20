2b4b2038a9bf711e84ffc69c4dcffef1e7e376e2
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-20 19:47:25 +0000

    Fix merging

--- src/Urls.ts
+++ src/Urls.ts
@@ -4,4 +4,5 @@ import {convertDate} from "./utils"
 export const URLS = {
     history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}`,
     metrics: (stock: string) => `https://financialmodelingprep.com/api/v3/company-key-metrics/${stock}?period=quarter`,
-    news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${s
+    news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${stock}&from=${before}&apiKey=${Secret.NEWS_API_KEY}`,
+    newsRaw: (stock:
