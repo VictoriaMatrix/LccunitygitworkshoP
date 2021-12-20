f7f4118545c51e0f60c2a14a4b29d3abc97dd7d6
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-20 06:29:06 +0000

    Added comments.

--- src/Urls.ts
+++ src/Urls.ts
@@ -3,4 +3,4 @@ import {convertDate} from "./utils"
 
 export const URLS = {
     history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}`,
-    metrics: (stock: string) => `https://f
+    metrics: (stock: string) => `https://financialmodelingprep.com/api/v3/company-key-metrics/${stock}?period=quarter
