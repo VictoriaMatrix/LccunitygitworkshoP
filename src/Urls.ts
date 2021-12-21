bb748db7b18abdc8a28c8bc2bde060196ba0b4cb
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-21 17:39:58 +0000

    iio: Fix IIO_EVENT_CODE_EXTRACT_DIR bit mask

--- src/Urls.ts
+++ src/Urls.ts
@@ -5,4 +5,5 @@ export const URLS = {
     history: (stock: string, from: Date, to: Date) => `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}`,
     metrics: (stock: string) => `https://financialmodelingprep.com/api/v3/company-key-metrics/${stock}?period=quarter`,
     news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${stock}&from=${before}&apiKey=${Secret.NEWS_API_KEY}`,
-    newsRaw: (stock: string, before: Date) => `https://news.google.com/search?q=${stock}
+    newsRaw: (stock: string, before: Date) => `https://news.google.com/search?q=${stock}+before:${convertDate(before)}&hl=en-US&gl=US&ceid=US:en`,
+    profile: (s
