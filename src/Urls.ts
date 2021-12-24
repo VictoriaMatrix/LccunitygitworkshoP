0ac89a2cfa146abc57e950486dc70ff4d164c142
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-24 06:19:26 +0000

    updated about page

--- src/Urls.ts
+++ src/Urls.ts
@@ -6,4 +6,5 @@ export const URLS = {
     metrics: (stock: string) => `https://financialmodelingprep.com/api/v3/company-key-metrics/${stock}?period=quarter`,
     news: (stock: string, before: Date) => `https://newsapi.org/v2/everything?q=${stock}&from=${before}&apiKey=${Secret.NEWS_API_KEY}`,
     newsRaw: (stock: string, before: Date) => `https://news.google.com/search?q=${stock}+before:${convertDate(before)}&hl=en-US&gl=US&ceid=US:en`,
-    profile: (stock: string) => `https://financialmodelingprep.com/api/v3/company/profile/$
+    profile: (stock: string) => `https://financialmodelingprep.com/api/v3/company/profile/${stock}`,
+}
