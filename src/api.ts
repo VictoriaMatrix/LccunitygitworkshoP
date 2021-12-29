4b8a2314a47a1c6a4e153dd1230ced9bbd6229cc
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-29 21:21:17 +0000

    Added legend

--- src/api.ts
+++ src/api.ts
@@ -13,4 +13,4 @@ const Urls = {
     history: (stock: string, from: Date, to: Date) =>
       `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}&apikey=${process.env.REACT_APP_API_KEY}`,
     list: () =>
-   
+    `https://financialmodelingprep.com/api/v3/company/stock/list?apikey=${pr
