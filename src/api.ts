0a692f997d2bb497e7e7a80196647f4e56b5fda1
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2021-12-30 09:03:23 +0000

    Remove print

--- src/api.ts
+++ src/api.ts
@@ -13,4 +13,6 @@ const Urls = {
     history: (stock: string, from: Date, to: Date) =>
       `https://financialmodelingprep.com/api/v3/historical-price-full/${stock}?from=${convertDate(from)}&to=${convertDate(to)}&apikey=${process.env.REACT_APP_API_KEY}`,
     list: () =>
-    `https://financialmodelingprep.com/api/v3/company/stock/list?apikey=${pr
+    `https://financialmodelingprep.com/api/v3/company/stock/list?apikey=${process.env.REACT_APP_API_KEY}`,
+    metrics: (stock: string) =>
+      `https
