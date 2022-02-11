eaf8f91f296bb057926c611e4b9557f599b404ea
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2022-02-11 06:17:52 +0000

    fix Makefile.am

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -2,4 +2,8 @@ import React, { useEffect } from 'react'
 import * as d3 from 'd3'
 
 const BarChart: React.FC = () => {
-    const canvas 
+    const canvas = React.createRef<HTMLDivElement>()
+    const data = [2,4,2,6,8]
+
+    useEffect(() => {
+   
