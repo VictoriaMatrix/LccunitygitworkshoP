a34bd86a963cc9ff62c1ef9137f29b059e507ddc
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2022-02-26 11:47:49 +0000

    zram: avoid access beyond the zram device

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,4 +3,8 @@ import React from "react"
 export const Footer: React.FC = () => {
     return(
         <footer className=".app-footer">
-            <div className="ml-au
+            <div className="ml-auto">
+                <span>Stox</span>
+            </div>
+        </footer>
+    )
