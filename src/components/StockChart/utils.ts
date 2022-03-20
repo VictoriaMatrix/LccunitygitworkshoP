384dbb4f8b7ba45edd3d74918d56e977b6647e70
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2022-03-20 08:30:50 +0000

    Added ignore.

--- src/components/StockChart/utils.ts
+++ src/components/StockChart/utils.ts
@@ -1,3 +1,6 @@
 import { flatten } from "lodash"
 import { Colors } from "../../Colors"
-import { StockNode }
+import { StockNode } from "../../types"
+import { seqArray, unique } from "../../utils"
+
+export 
