2de369ff652e1fccd31cf1702f2b220b461c8f28
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2022-03-23 08:16:01 +0000

    merged README

--- src/components/StockChart/utils.ts
+++ src/components/StockChart/utils.ts
@@ -6,4 +6,5 @@ import { seqArray, unique } from "../../utils"
 export const getChartData = (g: typeof google, data: StockNode[], predict: StockNode[][]) => {
     const actualData = new g.visualization.DataTable()
     actualData.addColumn({type: "date", label: "Date"})
-    ac
+    actualData.addColumn({type: "number", label: "Historic"})
+    data.forEach(n => ac
