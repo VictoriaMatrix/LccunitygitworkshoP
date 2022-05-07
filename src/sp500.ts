79e67e3016e4f56ea2b1df95d62116ea76c7ff58
VictoriaMatrix <119881478+VictoriaMatrix@users.noreply.github.com>
2022-05-07 12:18:25 +0000

    up test

--- src/sp500.ts
+++ src/sp500.ts
@@ -1,2 +1,2 @@
 // TODO: use endpoint
-export const sp500 = ["MMM","ABT","ABBV","ABMD",
+export const sp500 = ["MMM","ABT","ABBV","ABMD","ACN","ATVI","ADBE","AMD","AAP","AES","AMG","AFL","A","APD","AKAM","
